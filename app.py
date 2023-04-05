0372a3796e27a2ec5f01e9a1b2581a15a33ce7f5
Pulipaka, Srikar Kashyap <spulipa@iu.edu>
2023-04-05 00:09:45 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+import flask
+import openai
+import PyPDF2
+
+app = flask.Flask(__name__)
+
+openai.api_key = "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"
+
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route('/questions', methods=['POST'])
+def questions():
+    # read file and get the query
+    file = flask.request.files['resume']
+    pdfReader = PyPDF2.PdfReader(file)
+    resume = ""
+    for i in range(len(pdfReader.pages)):
+        resume += pdfReader.pages[i].extract_text()
+    content = """
+    {}
+--------------------------
+
+Given the above resume, generate 10 technical questions that can be asked to the candidate in an interview setting. Some of them have to be general and some specific. Assume mid-level candidate with 2-3 years of experience.
+""".format(resume)
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a principal data scientist/software engineer who is technically proficient."},
+            {"role": "user", "content": content}
+        ]
+    )
+    question = completion['choices'][0]['message']['content']
+    questions = question.split('\n')
+    # return str(answer)
+    return flask.render_template('questions.html', questions=questions)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
