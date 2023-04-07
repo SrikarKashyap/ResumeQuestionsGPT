5148957e824f44914a89600a3298226525d8fa3b
Pulipaka, Srikar Kashyap <spulipa@iu.edu>
2023-04-07 13:20:00 -0400

    new feature:local anomymization and time prto process

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import PyPDF2
 import json
 # from presidio_analyzer import AnalyzerEngine
 # from presidio_anonymizer import AnonymizerEngine
+import spacy
 import os
+import re
 import time
 
 
@@ -12,9 +14,9 @@ app = flask.Flask(__name__)
 
 openai.api_key = "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"
 
-# command = "python -m spacy download en_core_web_lg"
+command = "python -m spacy download en_core_web_sm"
 
-# os.system(command)
+os.system(command)
 
 
 @app.route('/')
@@ -22,7 +24,14 @@ def index():
     return flask.render_template('index.html')
 
 
-# def anonymize_text(text):
+def anonymize_text(text):
+    model = spacy.load("en_core_web_sm")
+    doc = model(text)
+    for ent in doc.ents:
+        if ent.label_ == "PERSON":
+            text = text.replace(ent.text, "John Doe")
+    return text
+
 #     analyzer = AnalyzerEngine()
 #     anonymizer = AnonymizerEngine()
 #     results = analyzer.analyze(text=text,
@@ -32,6 +41,32 @@ def index():
 #     anonymized_text = anonymizer.anonymize(text=text, analyzer_results=results)
 
 #     return anonymized_text.text
+
+
+def process_and_identify(text):
+    # regular expressions
+    email_re = r"[\w\.-]+@[\w\.-]+"
+    phone_re = r"\+?\d[\d -]{5,15}\d"
+    url_re = r"(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)"
+    ssn_re = r"\d{3}-\d{2}-\d{4}"
+    pan_re = r"[A-Z]{5}[0-9]{4}[A-Z]{1}"
+    aadhar_re = r"\d{4}\s\d{4}\s\d{4}"
+    # find all emails, phones and urls
+    emails = re.findall(email_re, text)
+    phones = re.findall(phone_re, text)
+    urls = re.findall(url_re, text)
+    ssns = re.findall(ssn_re, text)
+    pans = re.findall(pan_re, text)
+    aadhars = re.findall(aadhar_re, text)
+    # remove emails, phones and urls from text
+    text = re.sub(email_re, '<EMAIL>', text)
+    text = re.sub(phone_re, '<PHONE>', text)
+    text = re.sub(url_re, '<URL>', text)
+    text = re.sub(ssn_re, '<SSN>', text)
+    text = re.sub(pan_re, '<PAN>', text)
+    text = re.sub(aadhar_re, '<AADHAR>', text)
+
+    return text
 
 
 @app.route('/questions', methods=['POST'])
@@ -46,10 +81,16 @@ def questions():
         resume += pdfReader.pages[i].extract_text()
     end_pdf = time.time()
     durations['PDF Parsing'] = round(end_pdf - start_pdf, 3)
-    # start_anon = time.time()
+    start_anon = time.time()
+    resume = process_and_identify(resume)
+    resume = anonymize_text(resume)
+    end_anon = time.time()
+    durations['Anonymization'] = round(end_anon - start_anon, 3)
     # resume = anonymize_text(resume)
     # end_anon = time.time()
     # durations['anon'] = end_anon - start_anon
+    # with open('processed_resume.txt', 'w', encoding='utf8') as f:
+    #     f.write(resume)
     start_gpt = time.time()
     resume += "------------------\n"+additional_info+"\n------------------\n"
     content = """{resume}
@@ -79,8 +120,8 @@ Answer MUST be in JSON format with the following structure:
         ]
     )
     questions = completion['choices'][0]['message']['content']
-    with open('questions.json', 'w') as f:
-        f.write(questions)
+    # with open('questions.json', 'w') as f:
+    #     f.write(questions)
     try:
         answer = json.loads(questions)
     except Exception as e:
@@ -91,4 +132,4 @@ Answer MUST be in JSON format with the following structure:
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
